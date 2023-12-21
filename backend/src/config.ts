9869d59de361e5038ba76c65a7941661ea096fa6
Sahan Lakmal <sahanloser@gmail.com>
2023-12-22 00:24:13 +0530

    create http route and add mongoDB to project

---
+++ backend/src/config.ts
@@ -0,0 +1,2 @@
+export const PORT = 5555;
+export const mongoDBURL = 'mongodb+srv://root:root@todo-mern.wd1kva7.mongodb.net/todo-collection?retryWrites=true&w=majority'
---
+++ backend/src/main.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import {PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    console.log(req);
+    return res.status(234).send("Welcome to to do app");
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("App Connected to MongoDB");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log("Error connecting to the database", error);
+    });
+
